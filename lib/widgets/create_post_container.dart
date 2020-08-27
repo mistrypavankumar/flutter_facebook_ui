b1e8fb0d1bb041b13dfe7c55456f40ff7701dba8
mg143pavankumar <pavansharma.mg143@gmail.com>
2020-08-28 01:06:42 +0530

    Added header of fb

---
+++ lib/widgets/create_post_container.dart
@@ -0,0 +1,88 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/models/models.dart';
+
+class CreatePostContainer extends StatelessWidget {
+  final User currentUser;
+
+  const CreatePostContainer({
+    Key key,
+    @required this.currentUser,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 0.0),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              CircleAvatar(
+                radius: 20.0,
+                backgroundColor: Colors.grey[200],
+                backgroundImage:
+                    CachedNetworkImageProvider(currentUser.imageUrl),
+              ),
+              const SizedBox(width: 8.0),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration.collapsed(
+                      hintText: "What's on your mind?",
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                      )),
+                ),
+              ),
+            ],
+          ),
+          const Divider(
+            height: 10.0,
+            thickness: 0.5,
+          ),
+          Container(
+            height: 40.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                FlatButton.icon(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.videocam,
+                    color: Colors.red,
+                  ),
+                  label: Text('Live'),
+                ),
+                const VerticalDivider(
+                  width: 8.0,
+                  thickness: 0.5,
+                ),
+                FlatButton.icon(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.photo_library,
+                    color: Colors.lightGreen,
+                  ),
+                  label: Text('Photo'),
+                ),
+                const VerticalDivider(
+                  width: 8.0,
+                  thickness: 0.5,
+                ),
+                FlatButton.icon(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.video_call,
+                    color: Colors.purple,
+                  ),
+                  label: Text('Room'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
